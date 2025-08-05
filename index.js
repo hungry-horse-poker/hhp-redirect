408900c8c1fc252e5a102423d4e4eba8e37b95d7
Noah Boswell <noah@s56.net>
2025-08-04 21:50:38 -0600

    fixed issue with deployment

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('*', (req, res) => {
   }
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080rm -rf node_modules package-lock.json;
 app.listen(port, () => console.log(`Listening on port ${port}`));
