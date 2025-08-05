a5cc5f3b07e8ce10aa3529094229f8646ad4e7a9
Noah Boswell <noah@s56.net>
2025-08-04 21:40:16 -0600

    updated port

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('*', (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
